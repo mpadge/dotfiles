fc1ea0160ea9cb264aa100f68b0f95096002f108
mpadge <mark.padgham@email.com>
2021-09-15 13:27:59 +0200

    add help command to issues.bash

--- system/issues.bash
+++ system/issues.bash
@@ -1,7 +1,17 @@
 #!/usr/bin/bash
 
+NC='\033[0m'
+ARG='\033[0;31m' # red
+TXT='\033[0;32m' # green, or 1;32m for light green
+SYM='\u2192' # right arrow
+
 if [ "$1" == "" ]; then
     Rscript -e "dat <- mpmisc::latest_comments (); writeLines (dat$repo_issue, '~/Documents/issues')"
+elif [ "$1" == "help" ]; then
+    echo -e "${SYM} ${ARG}no arguments${NC}    : ${TXT}update issue comment list.${NC}"
+    echo -e "${SYM} ${ARG}help${NC}            : ${TXT}display these help messages.${NC}"
+    echo -e "${SYM} ${ARG}<repo>${NC}          : ${TXT}open GitHub issues page for <repo>.${NC}"
+    echo -e "${SYM} ${ARG}<repo> <number>${NC} : ${TXT}open GitHub issue#<number> for <repo>.${NC}"
 elif [ "$2" == "" ]; then
     Rscript -e "mpmisc::open_gh_issue (\"$1\")"
 else
