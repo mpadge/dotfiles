9a0d9205a486832e646a02fa45b788ae7c957b31
mpadge <mark.padgham@email.com>
2021-09-23 14:30:40 +0200

    add gh done command to gh_notifications.bash

--- system/gh_notifications.bash
+++ system/gh_notifications.bash
@@ -2,6 +2,8 @@
 
 if [ "$1" == "" ]; then
     Rscript -e "mpmisc::gh_notifications ()"
+else if [ "$1" == "done" ]; then
+    Rscript -e "mpmisc::mark_gh_notifications_as_read()"
 else
     Rscript -e "mpmisc::open_gh_notification ($1)"
 fi
