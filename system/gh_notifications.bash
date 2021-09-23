912880530863d61bd6e583466831bb652ee4cec6
mpadge <mark.padgham@email.com>
2021-09-23 14:31:53 +0200

    elif

--- system/gh_notifications.bash
+++ system/gh_notifications.bash
@@ -2,7 +2,7 @@
 
 if [ "$1" == "" ]; then
     Rscript -e "mpmisc::gh_notifications ()"
-else if [ "$1" == "done" ]; then
+elif [ "$1" == "done" ]; then
     Rscript -e "mpmisc::mark_gh_notifications_as_read()"
 else
     Rscript -e "mpmisc::open_gh_notification ($1)"
