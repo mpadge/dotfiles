f3ac4c51c524f33b7e55883da615193283d6d8b2
mpadge <mark.padgham@email.com>
2021-10-27 12:58:48 +0200

    add titles to himalaya output

--- system/himalaya.bash
+++ system/himalaya.bash
@@ -1,6 +1,17 @@
 #!/usr/bin/bash
 
+NC='\033[0m'
+TXT='\033[1;32m' # green, or 0;32m for darker green
+SYM='\u2192' # right arrow
+
+if [ "$1" == "" ]; then # check all
+    echo -e "${SYM} ${TXT}email${NC}"
+    himalaya -a email
+    echo -e "${SYM} ${TXT}email2${NC}"
+    himalaya -a email2
+    # ...
 if [ "$2" == "" ]; then
+    echo -e "${SYM} ${TXT}$1${NC}"
     himalaya -a $1
 elif [ "$2" == "d" ]; then
     himalaya -a $1 delete $3
