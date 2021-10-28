a0304b18244b5c5c94620b65441d61515713005c
mpadge <mark.padgham@email.com>
2021-10-28 11:21:43 +0200

    improve gh_notifications.bash

--- system/gh_notifications.bash
+++ system/gh_notifications.bash
@@ -4,6 +4,9 @@ if [ "$1" == "" ]; then
     Rscript -e "mpmisc::gh_notifications ()"
 elif [ "$1" == "done" ]; then
     Rscript -e "mpmisc::mark_gh_notifications_as_read()"
-else
+elif [[ "$1" =~ ^[0-9]+$ ]]; then
     Rscript -e "mpmisc::open_gh_notification ($1)"
+else
+    echo "gn only accepts 'done' or a single number"
+    exit 1
 fi
