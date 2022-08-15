dd1906d8d2bf52ac3ce82250260c306406ef829a
mpadge <mark.padgham@email.com>
2022-08-15 10:57:01 +0200

    update git-origin-open-url.bash script

--- system/git-origin-open-url.bash
+++ system/git-origin-open-url.bash
@@ -1,5 +1,16 @@
 #!/usr/bin/bash
 
-u=$(git remote -v | grep origin | head -1 | sed 's/^\w*//' | tr -s ' ' | cut -d ' ' -f 1)
-echo "opening $u"
-xdg-open $u &
+if ! git -C . rev-parse; then
+    exit 1
+fi
+
+uo=$(git remote -v | grep origin | head -1 | sed 's/^\w*//' | tr -s ' ' | cut -d ' ' -f 1)
+uu=$(git remote -v | grep upstream | head -1 | sed 's/^\w*//' | tr -s ' ' | cut -d ' ' -f 1)
+
+if [[ $uu ]]; then
+    echo "opening $uu"
+    xdg-open $uu &
+else
+    echo "opening $uo"
+    xdg-open $uo &
+fi
