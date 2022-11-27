e4745502014e917337f1ca015fa11a7d1d3b06dc
mpadge <mark.padgham@email.com>
2022-11-27 14:31:03 +0100

    change sshd to tailscale

--- system/sshd.bash
+++ system/sshd.bash
@@ -1,12 +1,14 @@
 #!/usr/bin/bash
 
-if [ "$1" == "" ]; then
-    sudo systemctl start sshd.service
+if [ "$1" == "" ] || [ "$1" == "start" ]; then
+    sudo systemctl start tailscaled.service
+    sudo tailscale up --ssh
 elif [ "$1" == "stop" ]; then
-    sudo systemctl stop sshd.service
+    sudo tailscale down
+    sudo systemctl stop tailscaled.service
 elif [ "$1" == "status" ]; then
-    sudo systemctl status sshd.service
+    tailscale status
 else
-    echo "ssdh only accepts 'stop' or 'status'"
+    echo "server only accepts 'start', 'stop' or 'status'"
     exit 1
 fi
