36b997b8f4f6ff0cd1bb7a112971b210b1ddccb2
mpadge <mark.padgham@email.com>
2023-06-07 09:30:06 +0200

    add printer status cmd

--- system/printer.bash
+++ system/printer.bash
@@ -5,14 +5,19 @@ ARG='\033[0;31m' # red
 TXT='\033[0;32m' # green, or 1;32m for light green
 SYM='\u2192' # right arrow
 
+PRINTER='Canon'
+
 if [ "$1" == "" ] || [ "$1" == "help" ]; then
     echo -e "${SYM} ${ARG}no arguments${NC} : ${TXT}Display these help messages.${NC}"
     echo -e "${SYM} ${ARG}on${NC}           : ${TXT}Turn printer services on.${NC}"
-    echo -e "${SYM} ${ARG}<number>${NC}     : ${TXT}Turn printer services off${NC}"
+    echo -e "${SYM} ${ARG}off${NC}          : ${TXT}Turn printer services off${NC}"
+    echo -e "${SYM} ${ARG}status${NC}       : ${TXT}Display status of printer services${NC}"
 elif [ "$1" == "on" ]; then
     sudo systemctl start cups.service avahi-daemon.service
 elif [ "$1" == "off" ]; then
     sudo systemctl stop cups.service avahi-daemon.service
+elif [ "$1" == "status" ]; then
+    avahi-browse --all --ignore-local --resolve --terminate | grep ${PRINTER}
 else
     echo "Parameter not recognised; see 'printer help' for details"
     exit 1
