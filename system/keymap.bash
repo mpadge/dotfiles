2daaf57ebd64c9eeed3aa79a0d7d8d60c9aada48
mpadge <mark.padgham@email.com>
2023-08-07 12:52:58 +0200

    update keymap.bash script

--- system/keymap.bash
+++ system/keymap.bash
@@ -1,14 +1,15 @@
 #!/usr/bin/bash
 
-# Set keymap for current X11 session, to enable easy temporary switching.
-
 if [ "$1" == "" ]; then
-    setxkbmap gb
+    #setxkbmap gb
+    setxkbmap us
 elif [ "$1" == "de" ]; then
     setxkbmap de
+elif [ "$1" == "c" ]; then
+    setxkbmap -v us -variant colemak_dh
 elif [ "$1" == "list" ]; then
     setxkbmap -print -verbose 10
 else
-    echo "keymap only accepts 'de' or 'list'"
+    echo "keymap only accepts 'de', 'c', or 'list'"
     exit 1
 fi
