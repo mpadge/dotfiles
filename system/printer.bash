9f6a1199af5070ba19ab117d618c1e39642321f5
mpadge <mark.padgham@email.com>
2023-10-30 11:36:57 +0100

    update printer.bash

--- system/printer.bash
+++ system/printer.bash
@@ -9,12 +9,12 @@ PRINTER='Canon'
 
 if [ "$1" == "" ] || [ "$1" == "help" ]; then
     echo -e "${SYM} ${ARG}no arguments${NC} : ${TXT}Display these help messages.${NC}"
-    echo -e "${SYM} ${ARG}on${NC}           : ${TXT}Turn printer services on.${NC}"
-    echo -e "${SYM} ${ARG}off${NC}          : ${TXT}Turn printer services off${NC}"
+    echo -e "${SYM} ${ARG}on/start${NC}     : ${TXT}Turn printer services on.${NC}"
+    echo -e "${SYM} ${ARG}off/stop${NC}     : ${TXT}Turn printer services off${NC}"
     echo -e "${SYM} ${ARG}status${NC}       : ${TXT}Display status of printer services${NC}"
-elif [ "$1" == "on" ]; then
+elif [ "$1" == "on" ] || [ "$1" == "start" ]; then
     sudo systemctl start cups.service avahi-daemon.service
-elif [ "$1" == "off" ]; then
+elif [ "$1" == "off" ] || [ "$1" == "stop" ]; then
     sudo systemctl stop cups.service avahi-daemon.service
 elif [ "$1" == "status" ]; then
     avahi-browse --all --ignore-local --resolve --terminate | grep ${PRINTER}
