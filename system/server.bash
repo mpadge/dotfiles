50feab2096811b75f63d32466cdf3a9018f42403
mpadge <mark.padgham@email.com>
2023-10-31 14:10:54 +0100

    improve server.bash

--- system/server.bash
+++ system/server.bash
@@ -7,18 +7,22 @@ SYM='\u2192' # right arrow
 
 MACHINE='machine-name'
 USER='user-name'
+MODEMURL='https://mymodel.myfritz.net:444/myfritz'
 
 if [ "$1" == "" ]; then
     ssh ${USER}@${MACHINE}
-elif [ "$1" == "stop" ]; then
+elif [ "$1" == "stop" ] || [ "$1" == "off" ]; then
     ssh -t ${USER}@${MACHINE} 'sudo systemctl poweroff'
-elif [ "$1" == "suspend" ]; then
+elif [ "$1" == "suspend" ] || [ "$1" == "sleep" ]; then
     ssh -t ${USER}@${MACHINE} 'sudo systemctl suspend'
+elif [ "$1" == "wake" ]; then
+    xdg-open ${MODEMURL} &
 elif [ "$1" == "help" ]; then
-    echo -e "${SYM} ${ARG}no arguments${NC} : ${TXT}Connect to ${MACHINE}.${NC}"
-    echo -e "${SYM} ${ARG}stop${NC}         : ${TXT}Turn off ${MACHINE}.${NC}"
-    echo -e "${SYM} ${ARG}suspend${NC}      : ${TXT}Suspend ${MACHINE}.${NC}"
-    echo -e "${SYM} ${ARG}help${NC}         : ${TXT}Display these help messages${NC}"
+    echo -e "${SYM} ${ARG}no arguments${NC}  : ${TXT}Connect to ${MACHINE}.${NC}"
+    echo -e "${SYM} ${ARG}stop/off${NC}      : ${TXT}Turn off ${MACHINE}.${NC}"
+    echo -e "${SYM} ${ARG}suspend/sleep${NC} : ${TXT}Suspend ${MACHINE}.${NC}"
+    echo -e "${SYM} ${ARG}wake${NC}          : ${TXT}Wake ${MACHINE}.${NC}"
+    echo -e "${SYM} ${ARG}help${NC}          : ${TXT}Display these help messages${NC}"
 else
     echo "Parameter not recognised; see '${MACHINE} help' for details"
     exit 1
