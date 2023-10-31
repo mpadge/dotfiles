344e49cf201033cd6e52b6cdf259128083b0cd00
mpadge <mark.padgham@email.com>
2023-10-31 14:26:30 +0100

    add 'server status' command

--- system/server.bash
+++ system/server.bash
@@ -9,8 +9,17 @@ MACHINE='machine-name'
 USER='user-name'
 MODEMURL='https://mymodel.myfritz.net:444/myfritz'
 
+STATUS=$(tailscale status | grep ${MACHINE})
+IS_SLEEPING='relay'
+
 if [ "$1" == "" ]; then
     ssh ${USER}@${MACHINE}
+elif [ "$1" == "status" ]; then
+    if grep -q "${IS_SLEEPING}" <<< "${STATUS}"; then
+        echo -e "${SYM} ${ARG}${MACHINE}${TXT} is sleeping${NC}"
+    else
+        echo -e "${SYM} ${ARG}${MACHINE}${TXT} is active${NC}"
+    fi
 elif [ "$1" == "stop" ] || [ "$1" == "off" ]; then
     ssh -t ${USER}@${MACHINE} 'sudo systemctl poweroff'
 elif [ "$1" == "suspend" ] || [ "$1" == "sleep" ]; then
@@ -19,6 +28,7 @@ elif [ "$1" == "wake" ]; then
     xdg-open ${MODEMURL} &
 elif [ "$1" == "help" ]; then
     echo -e "${SYM} ${ARG}no arguments${NC}  : ${TXT}Connect to ${MACHINE}.${NC}"
+    echo -e "${SYM} ${ARG}status${NC}        : ${TXT}Display status of ${MACHINE}${NC}"
     echo -e "${SYM} ${ARG}stop/off${NC}      : ${TXT}Turn off ${MACHINE}.${NC}"
     echo -e "${SYM} ${ARG}suspend/sleep${NC} : ${TXT}Suspend ${MACHINE}.${NC}"
     echo -e "${SYM} ${ARG}wake${NC}          : ${TXT}Wake ${MACHINE}.${NC}"
