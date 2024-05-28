47e639a118e1ebdbd10f66e0a8ba353acd6bb222
mpadge <mark.padgham@email.com>
2024-05-28 10:50:04 +0200

    suppress output of searxng.bash

--- system/searxng.bash
+++ system/searxng.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ! systemctl is-active docker.service; then
+if ! systemctl is-active docker.service > /dev/null; then
     echo "Docker service is not running. Starting it now..."
     sudo systemctl start docker.service
     sleep 2
@@ -9,10 +9,7 @@ fi
 if ! sudo docker ps -a | grep "searxng" > /dev/null; then
     echo "Searxng is not running. Starting it now..."
     sudo docker compose -f /usr/local/searxng-docker/docker-compose.yaml up -d
-
     sleep 2
-else
-    echo "Searxng is already running."
 fi
 
 xdg-open http://127.0.0.1:8080
