65cdc2ab0b3862eb62d610aab1bfb5bc20603303
mpadge <mark.padgham@email.com>
2024-05-28 10:55:56 +0200

    modify searxng to not require sudo if already running

--- system/searxng.bash
+++ system/searxng.bash
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if ! systemctl is-active docker.service > /dev/null; then
-    echo "Docker service is not running. Starting it now..."
-    sudo systemctl start docker.service
-    sleep 2
-fi
+if ! ps aux | grep "/usr/local/searxng" > /dev/null; then
+    if ! systemctl is-active docker.service > /dev/null; then
+        echo "Docker service is not running. Starting it now..."
+        sudo systemctl start docker.service
+        sleep 2
+    fi
 
-if ! sudo docker ps -a | grep "searxng" > /dev/null; then
-    echo "Searxng is not running. Starting it now..."
-    sudo docker compose -f /usr/local/searxng-docker/docker-compose.yaml up -d
-    sleep 2
+    if ! sudo docker ps -a | grep "searxng" > /dev/null; then
+        echo "Searxng is not running. Starting it now..."
+        sudo docker compose -f /usr/local/searxng-docker/docker-compose.yaml up -d
+        sleep 2
+    fi
 fi
 
 xdg-open http://127.0.0.1:8080
