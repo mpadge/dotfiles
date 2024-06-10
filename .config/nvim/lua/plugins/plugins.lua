632d5710c6983eb43d4333ee049b9d5dd3b02b8a
mpadge <mark.padgham@email.com>
2024-06-10 14:01:37 +0200

    tab completion for nvim-cmp

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -54,23 +54,64 @@ return {
     "R-nvim/cmp-r",
     {
         "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-emoji",
+        },
         config = function()
             require("cmp").setup({ sources = { { name = "nvim_lua" } } })
             require("cmp").setup({ sources = { { name = "cmp_r" } } })
             require("cmp_r").setup({})
+        end,
+        ---@param opts cmp.ConfigSchema
+        opts = function(_, opts)
+            local has_words_before = function()
+                unpack = unpack or table.unpack
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0
+                    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
+
+            local cmp = require("cmp")
+
+            opts.mapping = vim.tbl_extend("force", opts.mapping, {
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif vim.snippet.active({ direction = 1 }) then
+                        vim.schedule(function()
+                            vim.snippet.jump(1)
+                        end)
+                    elseif has_words_before() then
+                        cmp.complete()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif vim.snippet.active({ direction = -1 }) then
+                        vim.schedule(function()
+                            vim.snippet.jump(-1)
+                        end)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+            })
         end,
     },
     -- change some telescope options and a keymap to browse plugin files
     {
         "nvim-telescope/telescope.nvim",
         keys = {
-        -- add a keymap to browse plugin files
-        -- stylua: ignore
-        {
-          "<localleader>ff",
-          function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-          desc = "Find Plugin File",
-        },
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<localleader>ff",
+        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        desc = "Find Plugin File",
+      },
         },
         -- change some options
         opts = {
