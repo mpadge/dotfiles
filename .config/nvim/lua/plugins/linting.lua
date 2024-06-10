b20b0b980cb686c32d9074929469f71f9bc16502
mpadge <mark.padgham@email.com>
2024-06-10 18:16:41 +0200

    add more nvim plugin format files

---
+++ .config/nvim/lua/plugins/linting.lua
@@ -0,0 +1,23 @@
+return {
+    "mfussenegger/nvim-lint",
+    event = {
+        "BufReadPre",
+        "BufNewFile",
+    },
+    config = function()
+        local lint = require("lint")
+
+        lint.linters_by_ft = {
+            cpp = { "cpplint" },
+            javascript = { "eslint_d" },
+            typescript = { "eslint_d" },
+            javascriptreact = { "eslint_d" },
+            typescriptreact = { "eslint_d" },
+            svelte = { "eslint_d" },
+            python = { "pylint" },
+            json = { "jsonlint" },
+            md = { "markdownlint", "textlint", "write-good" },
+            txt = { "textlint", "write-good" },
+        }
+    end,
+}
