699cf60c05686ddc60c865877440796fc4f9d5ad
mpadge <mark.padgham@email.com>
2024-06-10 12:50:14 +0200

    more nvim plugins.lua

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,6 +1,18 @@
 vim.g.mapleader = ";"
 vim.g.maplocalleader = ","
 
+local width = vim.api.nvim_win_get_width(0)
+local width_float = tonumber(vim.fn.str2float(tostring(width)))
+local half_width = math.floor(width_float / 2)
+os.execute("export R_rconsole_width=" .. tostring(half_width))
+
 return {
   { "jalvesaq/Nvim-R" },
+  { "jalvesaq/cmp-nvim-r" },
+
+  require("cmp").setup({
+    sources = {
+      { name = "cmp_nvim_r" },
+    },
+  }),
 }
