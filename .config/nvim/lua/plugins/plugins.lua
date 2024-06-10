858e176803acefbd75d96219f2a600063b2cbd2b
mpadge <mark.padgham@email.com>
2024-06-10 13:41:24 +0200

    source nvim-cmp completions

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -55,6 +55,7 @@ return {
     {
         "hrsh7th/nvim-cmp",
         config = function()
+            require("cmp").setup({ sources = { { name = "nvim_lua" } } })
             require("cmp").setup({ sources = { { name = "cmp_r" } } })
             require("cmp_r").setup({})
         end,
