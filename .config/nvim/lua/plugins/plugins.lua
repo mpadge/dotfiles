4823cc1bd8c819bbe735707434d67c61d9045f1b
mpadge <mark.padgham@email.com>
2024-06-10 12:56:08 +0200

    update nvim Nvim-R -> R-nvim

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -7,12 +7,25 @@ local half_width = math.floor(width_float / 2)
 os.execute("export R_rconsole_width=" .. tostring(half_width))
 
 return {
-  { "jalvesaq/Nvim-R" },
-  { "jalvesaq/cmp-nvim-r" },
-
-  require("cmp").setup({
-    sources = {
-      { name = "cmp_nvim_r" },
+    {
+        "R-nvim/R.nvim",
+        lazy = false
     },
-  }),
+    {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+        config = function ()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "markdown", "markdown_inline", "r", "rnoweb" },
+            })
+        end
+    },
+    "R-nvim/cmp-r",
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            require("cmp").setup({ sources = {{ name = "cmp_r" }}})
+            require("cmp_r").setup({ })
+        end,
+    },
 }
