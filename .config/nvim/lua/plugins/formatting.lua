d530d457182a0129b62ec96c428586603d0d6f23
mpadge <mark.padgham@email.com>
2024-06-12 11:39:33 +0200

    fix conform formatter but turn off for now

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,45 +1,29 @@
 return {
-    "stevearc/conform.nvim",
-    enabled = true,
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local conform = require("conform")
-
-        conform.setup({
-            formatters_by_ft = {
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                javascriptreact = { "prettier" },
-                typescriptreact = { "prettier" },
-                svelte = { "prettier" },
-                css = { "prettier" },
-                html = { "prettier" },
-                json = { "prettier" },
-                yaml = { "prettier" },
-                markdown = { "prettier" },
-                graphql = { "prettier" },
-                lua = { "stylua" },
-                python = { "isort", "black" },
-            },
-            format_on_save = {
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500,
-            },
-            formatters = {
-                shfmt = {
-                    prepend_args = { "-i", "4" },
-                },
-            },
-            autoformat = false,
-        })
-
-        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-            conform.format({
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 500,
-            })
-        end, { desc = "Format file or range (in visual mode)" })
-    end,
+  "stevearc/conform.nvim",
+  enabled = false,
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      svelte = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+      graphql = { "prettier" },
+      lua = { "stylua" },
+      python = { "isort", "black" },
+    },
+    formatters = {
+      shfmt = {
+        prepend_args = { "-i", "4" },
+      },
+    },
+    autoformat = false,
+  },
 }
