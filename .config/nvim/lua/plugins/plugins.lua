7be2d4a301c5acdb46b7dc41b8507bb404cf8523
mpadge <mark.padgham@email.com>
2024-09-24 10:54:25 +0200

    add barbar.nvim

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -30,6 +30,19 @@ return {
             return opts
         end,
     },
+    {
+        "romgrk/barbar.nvim",
+        dependencies = {
+            "lewis6991/gitsigns.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        init = function() vim.g.barbar_auto_setup = false end,
+        opts = {
+            -- animation = true,
+            -- insert_at_start = true,
+            hide = {inactive = true, current = false, visible = true},
+        }
+    },
 
     -- add pyright and r_language_server to lspconfig
     {
