5ebcd2b72f3b69fccdda02762d9f59f4b1b7b2b0
mpadge <mark.padgham@email.com>
2024-10-25 10:53:01 +0200

    'master' -> 'main'

--- system/git-big-script.bash
+++ system/git-big-script.bash
@@ -1,3 +1,3 @@
 # https://blog.ostermiller.org/git-remove-from-history
-# This command lists largest 20 files in git history of *master* branch
-git rev-list master | while read rev; do git ls-tree -lr $rev  | cut -c54- | sed -r 's/^ +//g;'; done  | sort -u | perl -e 'while (<>) { chomp; @stuff=split("\t");$sums{$stuff[1]} += $stuff[0];} print "$sums{$_} $_\n" for (keys %sums);' | sort -rn | head -n 20
+# This command lists largest 20 files in git history of *main* branch
+git rev-list main | while read rev; do git ls-tree -lr $rev  | cut -c54- | sed -r 's/^ +//g;'; done  | sort -u | perl -e 'while (<>) { chomp; @stuff=split("\t");$sums{$stuff[1]} += $stuff[0];} print "$sums{$_} $_\n" for (keys %sums);' | sort -rn | head -n 20
