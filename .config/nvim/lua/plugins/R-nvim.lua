5803e3caf45966a59edfd33a0ac0309eafb68777
mpadge <mark.padgham@email.com>
2025-05-08 13:10:21 +0200

    add 'R-nvim/cmp-r' to R-nvim.lua plugins

--- .config/nvim/lua/plugins/R-nvim.lua
+++ .config/nvim/lua/plugins/R-nvim.lua
@@ -69,4 +69,15 @@ return {
             require("cmp_r").setup({ })
         end,
     },
+
+    {
+        "R-nvim/cmp-r",
+        {
+            "hrsh7th/nvim-cmp",
+            config = function()
+                require("cmp").setup({ sources = {{ name = "cmp_r" }}})
+                require("cmp_r").setup({})
+            end,
+        },
+    },
 }
