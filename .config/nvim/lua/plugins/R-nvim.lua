2720594a0de8910152d5a353ba92014e0353067b
mpadge <mark.padgham@email.com>
2025-05-08 12:51:22 +0200

    add nvim-cmp to config

--- .config/nvim/lua/plugins/R-nvim.lua
+++ .config/nvim/lua/plugins/R-nvim.lua
@@ -38,4 +38,35 @@ return {
         end,
         lazy = false,
     },
+
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                sources = {{ name = "cmp_r" }},
+                mapping = cmp.mapping.preset.insert({
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                    -- During auto-completion, press <Tab> to select the next item.
+                    ['<Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+                        elseif has_words_before() then
+                            cmp.complete()
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<S-Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                }),
+            })
+            require("cmp_r").setup({ })
+        end,
+    },
 }
