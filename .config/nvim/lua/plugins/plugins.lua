aa525e1c150780d43863ed54700cf7f575b5ad5c
mpadge <mark.padgham@email.com>
2025-05-08 08:07:50 +0200

    improve markdown-preview nvim config

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -252,7 +252,7 @@ return {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
+        build = ':call mkdp#util#install()',
     },
     {
 
