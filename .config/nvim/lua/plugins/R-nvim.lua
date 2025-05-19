935c9e9fa390550131b5c52d6c3683cce133cee0
mpadge <mark.padgham@email.com>
2025-05-19 09:28:14 +0200

    rearrange R-nvim.lua
    https://github.com/R-nvim/cmp-r/issues/16 attempted fix, but does not yet work

--- .config/nvim/lua/plugins/R-nvim.lua
+++ .config/nvim/lua/plugins/R-nvim.lua
@@ -4,41 +4,6 @@ local half_width = math.floor(width_float / 2)
 
 return {
 
-    {
-        "R-nvim/R.nvim",
-        config = function()
-            -- Create a table with the options to be passed to setup()
-            local opts = {
-                R_args = { "--quiet", "--no-save" },
-                hook = {
-                    on_filetype = function()
-                        -- This function will be called at the FileType event
-                        -- of files supported by R.nvim. This is an
-                        -- opportunity to create mappings local to buffers.
-                        vim.api.nvim_buf_set_keymap(0, "n", "<Space>", "<Plug>RDSendLine", {})
-                        vim.api.nvim_buf_set_keymap(0, "v", "<Space>", "<Plug>RSendSelection", {})
-                        vim.api.nvim_buf_set_keymap(0, "i", "`", "<Plug>RmdInsertChunk", {})
-                        vim.o.foldmethod = "expr"
-                        vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-                        -- vim.o.foldenable = false
-                    end,
-                },
-                min_editor_width = 72,
-                pdfviewer = "/usr/bin/okular",
-                rconsole_width = half_width,
-                disable_cmds = {
-                    "RCustomStart",
-                    "RSPlot",
-                    "RSaveClose",
-                },
-                config_tmux = true,
-                external_term = "tmux split-window -vh",
-            }
-            require("r").setup(opts)
-        end,
-        lazy = false,
-    },
-
     -- https://github.com/R-nvim/cmp-r/wiki
 
     -- Snippet Engine written in Lua
@@ -256,4 +221,40 @@ return {
             })
         end,
     },
+
+    {
+        "R-nvim/R.nvim",
+        config = function()
+            -- Create a table with the options to be passed to setup()
+            local opts = {
+                R_args = { "--quiet", "--no-save" },
+                hook = {
+                    on_filetype = function()
+                        -- This function will be called at the FileType event
+                        -- of files supported by R.nvim. This is an
+                        -- opportunity to create mappings local to buffers.
+                        vim.api.nvim_buf_set_keymap(0, "n", "<Space>", "<Plug>RDSendLine", {})
+                        vim.api.nvim_buf_set_keymap(0, "v", "<Space>", "<Plug>RSendSelection", {})
+                        vim.api.nvim_buf_set_keymap(0, "i", "`", "<Plug>RmdInsertChunk", {})
+                        vim.o.foldmethod = "expr"
+                        vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+                        -- vim.o.foldenable = false
+                    end,
+                },
+                min_editor_width = 72,
+                pdfviewer = "/usr/bin/okular",
+                rconsole_width = half_width,
+                disable_cmds = {
+                    "RCustomStart",
+                    "RSPlot",
+                    "RSaveClose",
+                },
+                config_tmux = true,
+                external_term = "tmux split-window -vh",
+            }
+            require("r").setup(opts)
+        end,
+        lazy = false,
+    },
+
 }
