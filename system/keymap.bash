3d39c88702ce19bd082c30d35476443349fcb7c5
mpadge <mark.padgham@email.com>
2025-05-29 13:05:29 +0200

    update keymap.bash

--- system/keymap.bash
+++ system/keymap.bash
@@ -1,13 +1,20 @@
 #!/usr/bin/bash
 
-if [ "$1" == "" ]; then
+if ([ "$1" == "" ] || [ "$1" == "help" ]); then
+    echo -e "${SYM} ${ARG}no arguments or 'help'${NC} : ${TXT}Display this help.${NC}"
+    echo -e "${SYM} ${ARG}us${NC}         : ${TXT}US keyboard${NC}"
+    echo -e "${SYM} ${ARG}gb${NC}         : ${TXT}GB keyboard.${NC}"
+    echo -e "${SYM} ${ARG}de${NC}     : ${TXT}DE keyboard${NC}"
+    echo -e "${SYM} ${ARG}c${NC}      : ${TXT}Colemak keyboard${NC}"
+elif [ "$1" == "us" ]; then
     setxkbmap us
 elif [ "$1" == "gb" ]; then
     setxkbmap gb
 elif [ "$1" == "de" ]; then
     setxkbmap de
 elif [ "$1" == "c" ]; then
-    setxkbmap -v us -variant colemak_dh
+    # setxkbmap -v us -variant colemak_dh
+    setxkbmap -v us -variant colemak
 elif [ "$1" == "list" ]; then
     setxkbmap -print -verbose 10
 else
