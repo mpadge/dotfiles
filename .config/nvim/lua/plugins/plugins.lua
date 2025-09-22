dfa9a583c8e95f46ceff8bcbe03a0459e7a6d604
mpadge <mark.padgham@email.com>
2025-09-22 13:21:30 +0200

    update nvim plugins.lua

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,3 +1,7 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+if true then return {} end
+
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
 -- In your plugin files, you can:
@@ -5,7 +9,6 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-
     -- add gruvbox
     { "ellisonleao/gruvbox.nvim" },
 
