3cea2c9fb1b7c90b7bc1895ccee6f369e73c8378
mpadge <mark.padgham@email.com>
2025-11-07 14:31:01 +0100

    add lsp.lua nvim plugin config

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,11 @@
+--- https://vi.stackexchange.com/a/45764
+return {
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            inlay_hints = {
+                enabled = false
+            },
+        }
+    }
+}
