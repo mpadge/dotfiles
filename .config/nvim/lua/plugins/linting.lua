a58f49d7d9144a11f553dffc40be0d998abcccfe
mpadge <mark.padgham@email.com>
2025-12-04 16:41:39 +0100

    fix syntax of linting.lua for ale options

--- .config/nvim/lua/plugins/linting.lua
+++ .config/nvim/lua/plugins/linting.lua
@@ -6,18 +6,17 @@ return {
             -- Configuration goes here.
             local g = vim.g
 
-            g.ale_ruby_rubocop_auto_correct_all = 1
+            g:ale_ruby_rubocop_auto_correct_all = 1
 
-            g.ale_linters = {
+            g:ale_linters = {
                 ruby = { "rubocop", "ruby" },
                 lua = { "lua_language_server" },
                 r = { "r_language_server" },
             }
 
-            g.ale_r_lintr_lint_package = 0
-            g.ale_r_lintr_options =
-            "lintr::linters_with_defaults(indentation_linter(4),function_left_parentheses_linter=NULL,commented_code_linter=NULL,object_usage_linter=NULL)"
-            g.ale_styler_options = "spaceout::spaceout_style"
+            g:ale_r_lintr_lint_package = 0
+            g:ale_r_lintr_options = 'lintr::linters_with_defaults(indentation_linter(4),function_left_parentheses_linter=NULL,commented_code_linter=NULL,object_usage_linter=NULL)'
+            g:ale_r_styler_options = 'spaceout::spaceout_style'
         end,
     },
     {
