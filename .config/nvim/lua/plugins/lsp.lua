de7e85bf007ebf7adce36e8cda2d6bb3dcd92b09
mpadge <mark.padgham@email.com>
2026-01-15 16:11:05 +0100

    disable nvim inlay hints from lsp for r files

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -6,6 +6,14 @@ return {
             inlay_hints = {
                 enabled = false
             },
-        }
+        },
+        config = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = { "r", "rmd", "quarto" },
+                callback = function(args)
+                    vim.lsp.inlay_hint.enable(false, { bufnr = args.buf })
+                end,
+            })
+        end
     }
 }
