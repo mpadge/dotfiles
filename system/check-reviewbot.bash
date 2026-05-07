ed974671141ff037f1488107b6b1edfbcc2d2a3e
mpadge <mark.padgham@email.com>
2026-05-07 10:51:23 +1000

    run 'check-reviewbot' every time if bot is down; otherwise only daily

--- system/check-reviewbot.bash
+++ system/check-reviewbot.bash
@@ -16,6 +16,7 @@ status=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
 echo "$TODAY" > "$BOTCHECK_FILE"
 
 if [[ "$status" != "404" ]]; then
+    rm -f "$BOTCHECK_FILE"
     echo -e "$YELLOW                _              _           _      _          _                  $NC"
     echo -e "$YELLOW _ _  ___  _ _ [_] ___  _ _ _ | |_  ___  _| |_   [_] ___   _| | ___  _ _ _  _ _ $NC"
     echo -e "$YELLOW| '_]/ ._]| | || |/ ._]| | | || . \/ . \  | |    | |[_-[  / . |/ . \| | | || ' |$NC"
