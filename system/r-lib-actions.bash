df676e6a2667175d5b0a08ea9d2ecc647548bbcf
mpadge <mark.padgham@email.com>
2026-06-19 10:44:26 +0200

    add conditional pkgdown workflow to r-lib-actions

--- system/r-lib-actions.bash
+++ system/r-lib-actions.bash
@@ -1,3 +1,6 @@
 rm .github/workflows/test-coverage.yaml .github/workflows/R-CMD-check.yaml
 Rscript -e "usethis::use_github_action('test-coverage')"
 Rscript -e "usethis::use_github_action('check-standard')"
+if [ -f .github/workflows/pkgdown.yaml ]; then
+    Rscript -e "usethis::use_github_action('pkgdown')"
+fi
