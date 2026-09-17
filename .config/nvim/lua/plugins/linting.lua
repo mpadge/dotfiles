10895606b9589fec1d1b21704552ccbfbb73c1df
mpadge <mark.padgham@email.com>
2026-09-17 09:35:40 +0200

    rm temp langaugeserver patch
    https://github.com/REditorSupport/languageserver/pull/768

--- .config/nvim/lua/plugins/linting.lua
+++ .config/nvim/lua/plugins/linting.lua
@@ -19,27 +19,6 @@ return {
             g.ale_r_lintr_lint_package = 0
             g.ale_r_lintr_options = 'lintr::linters_with_defaults(indentation_linter(4),function_left_parentheses_linter=NULL,commented_code_linter=NULL,object_usage_linter=NULL)'
             g.ale_r_styler_options = 'spaceout::spaceout_style'
-
-            -- workaround for https://github.com/REditorSupport/languageserver/issues/766
-            g.ale_r_languageserver_cmd = [[
-            if (requireNamespace("languageserver", quietly = TRUE)) {
-            local({
-                patch_to_json <- function() {
-                    ns <- asNamespace("languageserver")
-                    if (!is.null(self$error)) {
-                        payload <- list(jsonrpc = self$jsonrpc, id = self$id, error = self$error)
-                    } else {
-                        payload <- list(jsonrpc = self$jsonrpc, id = self$id, result = self$result)
-                    }
-                    get("response_to_json", envir = ns)(payload)
-                }
-                ns <- asNamespace("languageserver")
-                get("Response", envir = ns)$set("public", "to_json", patch_to_json, overwrite = TRUE)
-                get("ResponseErrorMessage", envir = ns)$set("public", "to_json", patch_to_json, overwrite = TRUE)
-                })
-            }
-            languageserver::run()
-            ]]
         end,
     },
     {
